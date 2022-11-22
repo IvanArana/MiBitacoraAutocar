21dd753189d6363d95f13c4850581ca87a710b96
IvanArana <iv4ng4mer01@gmail.com>
2022-11-22 14:35:03 -0500

    protofirma

--- config/adminlte.php
+++ config/adminlte.php
@@ -325,64 +325,64 @@ return [
             'icon' => 'fas fa-fw fa-user',
         ],
         [
-            'text' => 'change_password',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-lock',
+            'text' => 'Registrar Nuevo Usuario',
+            'url'  => '/register',
+            'icon' => 'fas fa-fw fa-user-plus',
         ],
-        [
-            'text'    => 'multilevel',
-            'icon'    => 'fas fa-fw fa-share',
-            'submenu' => [
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-                [
-                    'text'    => 'level_one',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'level_two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-            ],
-        ],
-        ['header' => 'labels'],
-        [
-            'text'       => 'important',
-            'icon_color' => 'red',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'warning',
-            'icon_color' => 'yellow',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'information',
-            'icon_color' => 'cyan',
-            'url'        => '#',
-        ],
+        // [
+        //     'text'    => 'multilevel',
+        //     'icon'    => 'fas fa-fw fa-share',
+        //     'submenu' => [
+        //         [
+        //             'text' => 'level_one',
+        //             'url'  => '#',
+        //         ],
+        //         [
+        //             'text'    => 'level_one',
+        //             'url'     => '#',
+        //             'submenu' => [
+        //                 [
+        //                     'text' => 'level_two',
+        //                     'url'  => '#',
+        //                 ],
+        //                 [
+        //                     'text'    => 'level_two',
+        //                     'url'     => '#',
+        //                     'submenu' => [
+        //                         [
+        //                             'text' => 'level_three',
+        //                             'url'  => '#',
+        //                         ],
+        //                         [
+        //                             'text' => 'level_three',
+        //                             'url'  => '#',
+        //                         ],
+        //                     ],
+        //                 ],
+        //             ],
+        //         ],
+        //         [
+        //             'text' => 'level_one',
+        //             'url'  => '#',
+        //         ],
+        //     ],
+        // ],
+        // ['header' => 'labels'],
+        // [
+        //     'text'       => 'important',
+        //     'icon_color' => 'red',
+        //     'url'        => '#',
+        // ],
+        // [
+        //     'text'       => 'warning',
+        //     'icon_color' => 'yellow',
+        //     'url'        => '#',
+        // ],
+        // [
+        //     'text'       => 'information',
+        //     'icon_color' => 'cyan',
+        //     'url'        => '#',
+        // ],
     ],
 
     /*
--- resources/views/actividad/edit.blade.php
+++ resources/views/actividad/edit.blade.php
@@ -77,6 +77,18 @@
                     </div>
 
                 </div>
+
+                <div id="signature-pad" class="m-signature-pad">
+                    <div class="m-signature-pad--body">
+                        <canvas style="border: 2px dashed #ccc"></canvas>
+                    </div>
+
+                    <div class="m-signature-pad--footer">
+                        <button type="button" class="btn btn-sm btn-warning" data-action="clear">Clear</button>
+                        <button type="button" class="btn btn-sm btn-primary" data-action="save">Save</button>
+                    </div>
+                </div>
+
                 <a href="/actividades" class="btn btn-secondary" tabindex="5">Cancelar</a>
                 <button type="submit" class="btn btn-primary" tabindex="4">Submit</button>
                 </form>
@@ -91,4 +103,54 @@
 @stop
 
 @section('js')
+<script src="https://cdn.jsdelivr.net/npm/signature_pad@2.3.2/dist/signature_pad.min.js"></script>
+
+<script>
+    $(function () {
+    var wrapper = document.getElementById("signature-pad"),
+    clearButton = wrapper.querySelector("[data-action=clear]"),
+    saveButton = wrapper.querySelector("[data-action=save]"),
+    canvas = wrapper.querySelector("canvas"),
+    signaturePad;
+
+  // Adjust canvas coordinate space taking into account pixel ratio,
+  // to make it look crisp on mobile devices.
+  // This also causes canvas to be cleared.
+    window.resizeCanvas = function () {
+    var ratio =  window.devicePixelRatio || 1;
+    canvas.width = canvas.offsetWidth * ratio;
+    canvas.height = canvas.offsetHeight * ratio;
+    canvas.getContext("2d").scale(ratio, ratio);
+    }
+
+    resizeCanvas();
+
+    signaturePad = new SignaturePad(canvas);
+
+    clearButton.addEventListener("click", function(event) {
+    signaturePad.clear();
+    });
+
+    saveButton.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    if (signaturePad.isEmpty()) {
+        alert("Por favor añada su firma.");
+    } else {
+    var dataUrl = signaturePad.toDataURL();
+    var image_data = dataUrl.replace(/^data:image\/(png|jpg);base64,/, "");
+
+    $.ajax({
+        url: '/save-signature',
+        type: 'POST',
+        data: {
+        image_data: image_data,
+        },
+        }).done(function() {
+        //
+        });
+    }
+    });
+});
+</script>
 @stop
